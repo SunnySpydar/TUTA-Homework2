24d7c4fd7b2506b50da6282fe5e29e2367a9e531
DianaJ <dianajasinskaite@gmail.com>
2021-11-09 00:00:55 +0200

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter first number:");
+            string Number1 = Console.ReadLine();
+            Console.WriteLine("Enter second number:");
+            string Number2 = Console.ReadLine();
+
+            
+            int result = int.Parse(Number1) + int.Parse(Number2);
+            Console.Write($"Total sum is: {result}");
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
